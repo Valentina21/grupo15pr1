3dae88c9f007639949eb572e1aabcf5df8ddee60
valentina21 <valentinaa.cabrera@gmail.com>
2015-03-28 19:09:55 -0200

    hola

---
+++ DataAccessLayer/DALEmployeesMock.cs
@@ -0,0 +1,56 @@
+﻿using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DALEmployeesMock : IDALEmployees
+    {
+        private List<Employee> employeesRepository = new List<Employee>()
+        {
+            new PartTimeEmployee(){HourlyDate = 100},
+            new PartTimeEmployee(){HourlyDate = 150},
+            new PartTimeEmployee(){HourlyDate = 200},
+            new PartTimeEmployee(){HourlyDate = 250},
+            new PartTimeEmployee(){HourlyDate = 300},
+            new FullTimeEmployee(){},
+            new FullTimeEmployee(){},
+            new FullTimeEmployee(){},
+            new FullTimeEmployee(){},
+            new FullTimeEmployee(){},
+        };
+
+        public void AddEmployee(Employee emp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return employeesRepository;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> SearchEmployees(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PresentationLayerConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogicLayer;
+
+
+namespace PresentationLayerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            IBLEmployees blHandler = new BLEmployees(new DataAccessLayer.DALEmployeesEF());
+
+            //your code goes here!
+            //use the blHandler reference to the business logic layer
+            //...
+            //...
+        }
+    }
+}
+
