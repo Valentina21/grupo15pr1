fff5b734f1542ce09b10faef63a8892e981538dc
agustin83 <agusandrade@live.com>
2015-04-02 16:13:59 -0300

    Con ejercicio 2

--- PresentationLayerConsole/Program.cs
+++ PresentationLayerConsole/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusinessLogicLayer;
+using System.Data.Entity;
 
 //Probando si anda
 //prueba2
@@ -20,6 +21,40 @@ namespace PresentationLayerConsole
             //use the blHandler reference to the business logic layer
             //...
             //...
+
+            using (var context = new InheritanceMappingContext())
+            {
+                FullTimeEmployee full = new FullTimeEmployee()
+                {
+                    Name = "Juan",
+                    Salary = 500,
+                    StartDate = DateTime.Today
+                };
+
+                context.Employees.Add(full);
+
+                context.SaveChanges();
+            }
+        }
+    }
+
+    public class InheritanceMappingContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FullTimeEmployee>().Map(m =>
+            {
+                m.MapInheritedProperties();
+                m.ToTable("FULL_TIME_EMP");
+            });
+
+            modelBuilder.Entity<PartTimeEmployee>().Map(m =>
+            {
+                m.MapInheritedProperties();
+                m.ToTable("PART_TIME_EMP");
+            });
         }
     }
 }
